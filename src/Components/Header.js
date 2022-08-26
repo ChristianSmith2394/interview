f7fb78aedfb647aa83f049e721b0b6644118a905
Christian <christiansmith2394@gmail.com>
2022-08-26 15:20:04 -0400

    adjusting styling for the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import logo from '../photos/logo.gif'
-import { Link } from "react-router-dom"
 import './Header.css'
 
 function Header() {
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,48 +1,59 @@
 import React from "react";
 import "./Sidebar.css";
-import { useState } from "react";
 
 function Sidebar() {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
   return (
     <div className="sidebar">
       <nav>
         <ul>
-          <a href="#">Metadata</a>
-            <li>Title</li>
-            <li>Revision History</li>
-            <li>Document Identifications</li>
-            <li>Roles</li>
-            <li>Locations</li>
-            <li>Parties</li>
-            <li>Responsible Parties</li>
-        </ul>
-        <ul>
-          <a href="#">Import Profile</a>
-            <li>Import Profile</li>
-        </ul>
-        <ul>
-          <a href="#">System Characteristics</a>
-            <li>System Identification</li>
-            <li>System Information Types</li>
-            <li>Security Impact Levels</li>
-            <li>Authorization Boundary & Data Flow</li>
-        </ul>
-        <ul>
-          <a href="#">System Implementation</a>
-          <li>Users</li>
-          <li>Components</li>
-          <li>Implemented Components</li>
-          <li>System Inventory</li>
-          <li>Remarks</li>
-        </ul>
-        <ul>
-          <a href="#">Control Implementation</a>
-          <li>Controls</li>
-        </ul>
-        <ul>
-          <a href="#">Back Matter</a>
-          <li>Citations</li>
-          <li>Resources</li>
+          {/* <li><a href="#">Metadata</a></li>
+            <li><a href="#">Title</a></li>
+            <li><a href="#">Revision History</a></li>
+            <li><a href="#">Document Identifications</a></li>
+            <li><a href="#">Roles</a></li>
+            <li><a href="#">Locations</a></li>
+            <li><a href="#">Parties</a></li>
+            <li><a href="#">Responsible Parties</a></li>
+          <li><a href="#">Import Profile</a></li>
+            <li><a href="#">Import Profile</a></li>
+          <li><a href="#">System Characteristics</a></li>
+            <li><a href="#">System Identification</a></li>
+            <li><a href="#">System Information Types</a></li>
+            <li><a href="#">Security Impact Levels</a></li>
+            <li><a href="#">Authorization Boundary & Data Flow</a></li>
+
+          <li><a href="#">System Implementation</a></li>
+          <li><a href="#">Users</a></li>
+          <li><a href="#">Components</a></li>
+          <li><a href="#">Implemented Components</a></li>
+          <li><a href="#">System Inventory</a></li>
+          <li><a href="#">Remarks</a></li>
+
+          <li><a href="#">Control Implementation</a></li>
+          <li><a href="#">Controls</a></li> */}
+
+          <li>
+            <button class="collapse__button">Back Matter</button>
+          </li>
+          <div className="content">
+            <a href="#">Citations</a>
+            <a href="#">Resources</a>
+          </div>
         </ul>
       </nav>
     </div>
