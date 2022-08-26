148c66d4d6d5c458969b12c79df072a4b9f29702
Christian <christiansmith2394@gmail.com>
2022-08-26 11:15:57 -0400

    header in place, styling needs to be adjusted to my taste

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import logo from '../photos/logo.gif'
+import { Link } from "react-router-dom"
 import './Header.css'
 
 function Header() {
   return (
     <div className='header'>
-      <img
-        className='header__logo'
-        src={logo}
-        alt="Header logo"
-      />
+        <img
+          className='header__logo'
+          src={logo}
+          alt="Header logo"
+        />
+      <div className='header__nav'>
+        <div className='nav__option'>
+          SSP
+        </div>
+        <div className='nav__option'>
+          SAP
+        </div>
+        <div className='nav__option'>
+          SAR
+        </div>
+        <div className='nav__option'>
+          POAM
+        </div>
+        <div className='nav__option'>
+          Logout
+        </div>
+      </div>
     </div>
   )
 }
