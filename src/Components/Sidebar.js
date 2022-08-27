471cd2d8d3ce3caa5e62afeb06739a57c9aeb2b4
Christian <christiansmith2394@gmail.com>
2022-08-27 11:06:04 -0400

    everything is completed, just getting rid of a sliver of white space above the footer

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,58 +2,92 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
-  }
-
   return (
     <div className="sidebar">
       <nav>
         <ul>
-          {/* <li><a href="#">Metadata</a></li>
-            <li><a href="#">Title</a></li>
-            <li><a href="#">Revision History</a></li>
-            <li><a href="#">Document Identifications</a></li>
-            <li><a href="#">Roles</a></li>
-            <li><a href="#">Locations</a></li>
-            <li><a href="#">Parties</a></li>
-            <li><a href="#">Responsible Parties</a></li>
-          <li><a href="#">Import Profile</a></li>
-            <li><a href="#">Import Profile</a></li>
-          <li><a href="#">System Characteristics</a></li>
-            <li><a href="#">System Identification</a></li>
-            <li><a href="#">System Information Types</a></li>
-            <li><a href="#">Security Impact Levels</a></li>
-            <li><a href="#">Authorization Boundary & Data Flow</a></li>
-
-          <li><a href="#">System Implementation</a></li>
-          <li><a href="#">Users</a></li>
-          <li><a href="#">Components</a></li>
-          <li><a href="#">Implemented Components</a></li>
-          <li><a href="#">System Inventory</a></li>
-          <li><a href="#">Remarks</a></li>
-
-          <li><a href="#">Control Implementation</a></li>
-          <li><a href="#">Controls</a></li> */}
+          <li>
+            <a href="#">Metadata</a>
+          </li>
+          <ul>
+            <a href="#">Title</a>
+          </ul>
+          <ul>
+            <a href="#">Revision History</a>
+          </ul>
+          <ul>
+            <a href="#">Document Identifications</a>
+          </ul>
+          <ul>
+            <a href="#">Roles</a>
+          </ul>
+          <ul>
+            <a href="#">Locations</a>
+          </ul>
+          <ul>
+            <a href="#">Parties</a>
+          </ul>
+          <ul>
+            <a href="#">Responsible Parties</a>
+          </ul>
+          <li>
+            <a href="#">Import Profile</a>
+          </li>
+          <ul>
+            <a href="#">Import Profile</a>
+          </ul>
+          <li>
+            <a href="#">System Characteristics</a>
+          </li>
+          <ul>
+            <a href="#">System Identification</a>
+          </ul>
+          <ul>
+            <a href="#">System Information Types</a>
+          </ul>
+          <ul>
+            <a href="#">Security Impact Levels</a>
+          </ul>
+          <ul>
+            <a href="#">Authorization Boundary & Data Flow</a>
+          </ul>
 
           <li>
-            <button class="collapse__button">Back Matter</button>
+            <a href="#">System Implementation</a>
           </li>
-          <div className="content">
+          <ul>
+            <a href="#">Users</a>
+          </ul>
+          <ul>
+            <a href="#">Components</a>
+          </ul>
+          <ul>
+            <a href="#">Implemented Components</a>
+          </ul>
+          <ul>
+            <a href="#">System Inventory</a>
+          </ul>
+          <ul>
+            <a href="#">Remarks</a>
+          </ul>
+
+          <li>
+            <a href="#">Control Implementation</a>
+          </li>
+          <ul>
+            <a href="#">Controls</a>
+          </ul>
+
+          <li>
+            <a href="#">Back Matter</a>
+          </li>
+
+          <ul>
             <a href="#">Citations</a>
+          </ul>
+          <ul>
             <a href="#">Resources</a>
-          </div>
+          </ul>
         </ul>
       </nav>
     </div>
